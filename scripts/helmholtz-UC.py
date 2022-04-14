0c42b3c55461b9ed8e8ed77bef21e564f2add85f
Janosch Preuss <janosch.preuss@stud.uni-goettingen.de>
2022-04-14 13:11:49 +0100

    -improve marking of subdomains -add non-convex direction -fix meshwidth

--- scripts/helmholtz-UC.py
+++ scripts/helmholtz-UC.py
@@ -6,15 +6,15 @@ This script implements a modified version of the method presented in Chapter 4 o
 import numpy as np
 import ufl
 from dolfinx import fem, io, mesh, plot
-from ufl import ds, dx, grad, div,inner, jump, FacetNormal, dot, dS, Circumradius, triangle, FiniteElement, MixedElement
+from ufl import ds, dx, grad, div,inner, jump, FacetNormal, dot, dS, Circumradius, triangle, FiniteElement, MixedElement, CellDiameter
 from mpi4py import MPI
 from petsc4py.PETSc import ScalarType
 from math import log,pi
-from meshes import get_mesh_hierarchy
-
+from meshes import get_mesh_hierarchy, get_mesh_hierarchy_nonconvex
+from dolfinx.io import XDMFFile
 
 # problem data. Todo: put into class
-kk = ScalarType(10)
+kk = ScalarType(1)
 k2 = kk**2
 
 order = 1
@@ -24,12 +24,63 @@ x_l = 0.1-tol
 x_r = 0.9+tol
 y_b = 0.25-tol
 y_t = 1.0+tol
+
+
+## convex case 
+'''
 def omega_Ind(x):
-    tmp = ( x[0] < (0.1+tol) ) | ( x[0] > (0.9-tol) ) | ( x[1] < ( 0.25+tol ) ) 
-    return tmp
+    
+    values = np.zeros(x.shape[1],dtype=ScalarType)
+    omega_coords = np.logical_or( ( x[0] <= 0.1 ), 
+        np.logical_or(   (x[0] >= 0.9 ), (x[1] <= 0.25)  )
+        ) 
+    rest_coords = np.invert(omega_coords)
+    values[omega_coords] = np.full(sum(omega_coords), 1.0)
+    values[rest_coords] = np.full(sum(rest_coords), 0)
+    return values
 
 def B_Ind(x):
-    return  (x[0] > (0.1-tol) ) &  (x[0] < (0.9+tol) ) & (x[1] > (0.25-tol) ) &  (x[1] < (0.95+tol) ) 
+    values = np.zeros(x.shape[1],dtype=ScalarType)
+    # Create a boolean array indicating which dofs (corresponding to cell centers)
+    # that are in each domain
+    rest_coords = np.logical_and( ( x[0] >= 0.1 ), 
+        np.logical_and(   (x[0] <= 0.9 ),
+          np.logical_and(   (x[1]>= 0.95),  (x[1]<= 1+tol)  )
+        )
+      ) 
+    B_coords = np.invert(rest_coords)
+    values[B_coords] = np.full(sum(B_coords), 1.0)
+    values[rest_coords] = np.full(sum(rest_coords), 0)
+    return values
+'''
+
+## convex-case 
+def omega_Ind(x):
+    
+    values = np.zeros(x.shape[1],dtype=ScalarType)
+    omega_coords = np.logical_and( ( x[0] >= 0.25 ), 
+                     np.logical_and(   (x[0] <= 0.75 ),
+                       np.logical_and(   (x[1]>= 0.05),  (x[1]<= 0.5)  )
+                     )
+                   ) 
+    rest_coords = np.invert(omega_coords)
+    values[omega_coords] = np.full(sum(omega_coords), 1.0)
+    values[rest_coords] = np.full(sum(rest_coords), 0)
+    return values
+
+def B_Ind(x):
+    values = np.zeros(x.shape[1],dtype=ScalarType)
+    # Create a boolean array indicating which dofs (corresponding to cell centers)
+    # that are in each domain
+    B_coords = np.logical_and( ( x[0] >= 0.125 ), 
+                 np.logical_and(   (x[0] <= 0.875 ),
+                   np.logical_and(   (x[1]>= 0.05),  (x[1]<= 0.95)  )
+                 )
+               ) 
+    rest_coords = np.invert(B_coords)
+    values[B_coords] = np.full(sum(B_coords), 1.0)
+    values[rest_coords] = np.full(sum(rest_coords), 0)
+    return values
 
 sigma_x = ScalarType(0.01)
 sigma_y = ScalarType(0.1)
@@ -44,30 +95,36 @@ def SolveProblem(msh,order=1,export_VTK=False,sol_which=1,add_bc=False):
     #                        cell_type=mesh.CellType.triangle,)
     
     n = FacetNormal(msh)
-    h = 2.0*Circumradius(msh)
+    h = CellDiameter(msh)
+    #h = 2.0*Circumradius(msh)
     
     x = ufl.SpatialCoordinate(msh)
 
     Lu = lambda u : -div(grad(u)) - k2*u 
     
     if sol_which == 1:
-        pgamma = ScalarType(1e-5)
+        #pgamma = ScalarType(1e-5)
+        pgamma = ScalarType(1e-4)
         ue = ufl.exp( -( (x[0] - x_s) ** 2 / (2*sigma_x)  + (x[1] - y_s) ** 2 / (2*sigma_y)  ) )
         f =   -ue * ( (x[0]-x_s)**2/ sigma_x**2  + (x[1]-y_s)**2 / sigma_y**2 - 1/sigma_x - 1/sigma_y + k2 )
         palpha = ScalarType(1e-5)
     else: 
+        #pgamma = ScalarType(5e-3)
         pgamma = ScalarType(5e-3)
+        #pgamma = ScalarType(1e-4)
         #pgamma = ScalarType(1e-1)
         ue = ufl.sin(kk*pi*x[0]) * ufl.sin(kk*pi*x[1])
-        f = Lu(ue)
-        palpha = ScalarType(1e-2)
-        #f =   -ue *  (-2*(kk*pi)**2 + k2) 
+        #f = Lu(ue)
+        palpha = ScalarType(5e-3)
+        #palpha = ScalarType(1e-2)
+        f =   -ue *  (-2*(kk*pi)**2 + k2) 
 
      
     p_cg_order = FiniteElement('CG', msh.ufl_cell(), order)
+    #p_cg_order_dual = FiniteElement('CG', msh.ufl_cell(), order)
     mel = MixedElement([p_cg_order,p_cg_order])
-    #V = fem.FunctionSpace(msh, ("CG", 1))
     VW = fem.FunctionSpace(msh,mel)
+    #VW = fem.FunctionSpace(msh, p_cg_order_primal * p_cg_order_dual )
     ndof = VW.sub(0).dofmap.index_map.size_global * VW.sub(0).dofmap.index_map_bs 
 
     facets = mesh.locate_entities_boundary(msh, dim=1,
@@ -85,54 +142,104 @@ def SolveProblem(msh,order=1,export_VTK=False,sol_which=1,add_bc=False):
 
     
     Q_ind = fem.FunctionSpace(msh, ("DG", 0))
-
+    
     omega_ind = fem.Function(Q_ind)
-    cells_omega = mesh.locate_entities(msh, msh.topology.dim, omega_Ind)
-    omega_ind.x.array[:] = 0.0
-    omega_ind.x.array[cells_omega] = np.full(len(cells_omega), 1)
-
     B_ind = fem.Function(Q_ind)
-    cells_B = mesh.locate_entities(msh, msh.topology.dim, B_Ind)
-    B_ind.x.array[:] = 0.0
-    B_ind.x.array[cells_B] = np.full(len(cells_B), 1)
+    B_ind.interpolate(B_Ind)
+    omega_ind.interpolate(omega_Ind)
+
+    #cells_omega = mesh.locate_entities(msh, msh.topology.dim, omega_Ind)
+    #omega_ind.x.array[:] = 0.0
+    #omega_ind.x.array[cells_omega] = np.full(len(cells_omega), 1)
+
+    #cells_B = mesh.locate_entities(msh, msh.topology.dim, B_Ind)
+    #B_ind.x.array[:] = 0.0
+    #B_ind.x.array[cells_B] = np.full(len(cells_B), 1)
 
     #omega_ind = ufl.conditional(ufl.Not(ufl.And(ufl.And(x[0] >= 0.1, x[0] <= 0.9),ufl.And(x[1] >= 0.25, x[1] <= 1))   ), 1, 0) 
     #B_ind = ufl.conditional( ufl.Not(ufl.And(ufl.And(x[0] >= 0.1, x[0] <= 0.9), ufl.And(x[1] >= 0.95, x[1] <= 1))) , 1, 0 ) 
     #B_ind = ufl.conditional( ufl.Not(ufl.And(ufl.And(x[0] >= 0.1, x[0] <= 0.9), ufl.And(x[1] >= 0.9, x[1] <= 1))) , 1, 0 ) 
     
+    #px = 1e+1
     #input("") 
     a = omega_ind * u * v * dx
+    #a += pgamma*0.5*(h('+')+h('-'))*dot(jump(grad(u)),jump(grad(v)))*dS
     a += pgamma*0.5*(h('+')+h('-'))*dot(jump(grad(u),n),jump(grad(v),n))*dS
-    #a += pgamma*h('+')*dot(jump(grad(u)),jump(grad(v)))*dS
     a += pgamma * h**2 * Lu(u) * Lu(v) * dx 
-    #a += pgamma * h**2 * k2**2 * u * v * dx 
+    #a += pgamma * h**2 * Lu(u) * Lu(v) * dx 
     a += ( inner(grad(v), grad(z)) - k2*v*z) * dx 
     a -= inner(grad(z), grad(w)) * dx 
     a += ( inner(grad(u), grad(w)) - k2*u*w) * dx 
     a += palpha * h**(2*order) * u * v * dx
+    
+    #a += 1e-8 * u * v * dx
+    #a += 1e-8 * w * z * dx
 
     L = inner(f, w) * dx + omega_ind * ue  * v * dx  + pgamma * h**2 * inner(f,Lu(v)) * dx 
 
-    problem = fem.petsc.LinearProblem(a, L, bcs=bcs, petsc_options={"ksp_type": "preonly", "pc_type": "lu"})
+    #problem = fem.petsc.LinearProblem(a, L, bcs=bcs, petsc_options={"ksp_type": "preonly", "pc_type": "lu"})
+    problem = fem.petsc.LinearProblem(a, L, bcs=bcs, petsc_options={"ksp_type": "preonly", "pc_type": "lu","pc_factor_mat_solver_type":"mumps"})
     sol = problem.solve()
+    #print("sol = ",  sol.x.array)
+    #print(" len( sol.x.array ) = ", len( sol.x.array ) )  
+    #print("") 
+    
     uh,zh = sol.split() 
+    #print("uh = ",  uh.x.array)
+    #print(" len( uh.x.array ) = ", len( uh.x.array ) )  
+    #print("") 
+    
+    #print("zh = ",  zh.x.array)
+    #print(" len( zh.x.array ) = ", len( zh.x.array ) )  
+    #print("") 
+
     uh.name ="uh"
     zh.name ="zh"
 
     #uex = fem.Function(V)
     #u_exact = lambda x: np.exp( -( (x[0] - x_s) ** 2 / (2*sigma_x)  + (x[1] - y_s) ** 2 / (2*sigma_y)  ) )
     #uex.interpolate(u_exact)
-    L2_error = fem.form( (omega_ind+B_ind)*inner(uh - ue, uh - ue) * dx)
-    L2_norm = fem.form((omega_ind+B_ind)*inner(ue, ue) * dx)
+    
+    #L2_error = fem.form( (omega_ind+B_ind)*inner(uh - ue, uh - ue) * dx)
+    #L2_norm = fem.form((omega_ind+B_ind)*inner(ue, ue) * dx)
+    
+    L2_error = fem.form( (B_ind)*inner(uh - ue, uh - ue) * dx)
+    L2_norm = fem.form((B_ind)*inner(ue, ue) * dx)
+    
     error_local = fem.assemble_scalar(L2_error)
     L2_local = fem.assemble_scalar(L2_norm)
     error_L2 = np.sqrt(msh.comm.allreduce(error_local, op=MPI.SUM)) / np.sqrt(msh.comm.allreduce(L2_local, op=MPI.SUM)) 
     print("ndof = {0}, error_L2 = {1} ".format(ndof,error_L2)) 
 
     if export_VTK:
-        with io.XDMFFile(msh.comm, "helmholtz-UC-uh.xdmf", "w") as file:
+        #V = VW.sub(0).collapse()[0]
+        #V = VW.sub(0).collapse()
+        #p_cg_order = FiniteElement('CG', msh.ufl_cell(), order)
+        #V = fem.FunctionSpace(msh,p_cg_order)
+        V, V_to_VW = VW.sub(0).collapse()
+        #ndof_V = V.dofmap.index_map.size_global * V.dofmap.index_map_bs 
+        #print("ndof_V = ", ndof_V)
+        #print(" zh.x.array =  ", zh.x.array )   
+        #V = fem.FunctionSpace(msh, ("CG", order))
+        uex = fem.Function( V )
+        u_expr = fem.Expression(ue, V.element.interpolation_points)
+        uex.interpolate(u_expr)
+        #uex.interpolate(ue )
+        udiff = fem.Function(V)
+        #print(uh.x.array) 
+        #print(uex.x.array) 
+        #print(V_to_VW)
+        #input("")
+        udiff.x.array[:] = np.abs(uh.x.array[V_to_VW] - uex.x.array)
+        #udiff.x.array[:] = uh.x.array[V_to_VW]
+        with XDMFFile(msh.comm, "helmholtz-UC.xdmf", "w") as file:
             file.write_mesh(msh)
-            file.write_function(uh)
+            file.write_function(udiff)
+        #with io.XDMFFile(msh.comm, "helmholtz-UC-uh.xdmf", "w") as file:
+            #file.write_mesh(msh)
+            #file.write_function(uh)
+            #file.write_function( uh.sub(0).collapse() )
+            #file.write_function( udiff )
     
     return error_L2,ndof
 
@@ -145,17 +252,20 @@ l2_errors = [ ]
 
 import matplotlib.pyplot as plt 
 #ls_mesh = get_mesh_hierarchy(6)
-ls_mesh = get_mesh_hierarchy(7)
+#ls_mesh = get_mesh_hierarchy(6)
+ls_mesh = get_mesh_hierarchy_nonconvex(7)
+
 l2_errors_order = { }
 eoc_order = {  }
 h_order = {  }
 for order in [1,2,3]:
+#for order in [1]:
     print("order = {0}".format(order))
     ndofs = [] 
     l2_errors = [ ]
     for msh in ls_mesh[:-order]:
-        l2_error, ndof = SolveProblem(msh=msh,order=order)
-        #l2_error, ndof = SolveProblem(msh=msh,order=order, export_VTK=False,sol_which=2,add_bc=False)
+        #l2_error, ndof = SolveProblem(msh=msh,order=order)
+        l2_error, ndof = SolveProblem(msh=msh,order=order, export_VTK=True,sol_which=2,add_bc=False)
         #l2_errors.append(SolveProblem(N_mesh=N_mesh,order=1, export_VTK=False,sol_which=2,add_bc=False))
         #l2_errors.append(SolveProblem(msh=mesh,order=1, export_VTK=False,sol_which=2,add_bc=True))
         l2_errors.append(l2_error)
@@ -207,3 +317,19 @@ plt.show()
 #plt.ylabel("L2-error")
 #plt.show()
 
+'''
+def omega_Ind(x):
+    tmp = ( x[0] < (0.1+tol) ) | ( x[0] > (0.9-tol) ) | ( x[1] < ( 0.25+tol ) ) 
+    return tmp
+
+def B_Ind(x):
+    return  (x[0] > (0.1-tol) ) &  (x[0] < (0.9+tol) ) & (x[1] > (0.25-tol) ) &  (x[1] < (0.95+tol) ) 
+
+def omega_Ind(x):
+    tmp = ( ( x[0] > (0.25-tol) ) & ( x[0] < (0.75+tol) ) & ( x[1] > ( 0.05-tol ) ) & ( x[1] < ( 0.5+tol ) ) ) 
+    return tmp
+
+def B_Ind(x):
+    tmp = ( ( x[0] > (0.125-tol) ) & ( x[0] < (0.875+tol) ) & ( x[1] > ( 0.05-tol ) ) & ( x[1] < ( 0.95+tol ) ) ) 
+    return tmp
+'''
--- scripts/meshes.py
+++ scripts/meshes.py
@@ -123,36 +123,44 @@ def get_mesh_hierarchy(n_ref):
 
 '''
 ls_mesh = get_mesh_hierarchy(6)
-tol = 1e-13
+tol = 1e-12
 x_l = 0.1-tol
 x_r = 0.9+tol
 y_b = 0.25-tol
 y_t = 1.0+tol
+
 def omega_Ind(x):
-    #tmp =  Not(And(And(x[0] >= 0.1, x[0] <= 0.9),And(x[1] >= 0.25, x[1] <= 1))   )
-    #tmp =  ~( ( x[0] > (0.1 - tol) ) & ( x[0] < (0.9+tol) ) & ( x[1]>(0.25-tol) ) & ( x[1]<(1.0+tol) ) ) 
-    #tmp =  ~( ( x[0] > x_l ) & ( x[0] < x_r ) & ( x[1]>y_b ) & ( x[1]< y_t ) ) 
-    tmp = ( x[0] < (0.1+tol) ) | ( x[0] > (0.9-tol) ) | ( x[1] < ( 0.25+tol ) ) 
-    #tmp =  ( ( x[1] >= (0.5) )  ) 
-    return tmp
+    
+    values = np.zeros(x.shape[1],dtype=ScalarType)
+    omega_coords = np.logical_or( ( x[0] <= 0.1 ), 
+        np.logical_or(   (x[0] >= 0.9 ), (x[1] <= 0.25)  )
+        ) 
+    rest_coords = np.invert(omega_coords)
+    values[omega_coords] = np.full(sum(omega_coords), 1.0)
+    values[rest_coords] = np.full(sum(rest_coords), 0)
+    return values
 
 def B_Ind(x):
-    #return ~( ( x[0] > (0.1 - tol) ) & ( x[0] < (0.9+tol) ) & ( x[1]>(0.95-tol) ) & ( x[1]<(1+tol) ) ) 
-    return  (x[0] > (0.1-tol) ) &  (x[0] < (0.9+tol) ) & (x[1] > (0.25-tol) ) &  (x[1] < (0.95+tol) ) 
+    values = np.zeros(x.shape[1],dtype=ScalarType)
+    # Create a boolean array indicating which dofs (corresponding to cell centers)
+    # that are in each domain
+    rest_coords = np.logical_and( ( x[0] >= 0.1 ), 
+        np.logical_and(   (x[0] <= 0.9 ),
+          np.logical_and(   (x[1]>= 0.95),  (x[1]<= 1+tol)  )
+        )
+      ) 
+    B_coords = np.invert(rest_coords)
+    values[B_coords] = np.full(sum(B_coords), 1.0)
+    values[rest_coords] = np.full(sum(rest_coords), 0)
+    return values
 
 for idx,mesh in enumerate(ls_mesh):
 
     Q_ind = FunctionSpace(mesh, ("DG", 0))
-
+    B_ind  = Function(Q_ind)
     omega_ind = Function(Q_ind)
-    cells_omega = locate_entities(mesh, mesh.topology.dim, omega_Ind)
-    omega_ind.x.array[:] = 0.0
-    omega_ind.x.array[cells_omega] = np.full(len(cells_omega), 1)
-
-    B_ind = Function(Q_ind)
-    cells_B = locate_entities(mesh, mesh.topology.dim, B_Ind)
-    B_ind.x.array[:] = 0.0
-    B_ind.x.array[cells_B] = np.full(len(cells_B), 1)
+    B_ind.interpolate(B_Ind)
+    omega_ind.interpolate(omega_Ind)
 
     with XDMFFile(mesh.comm, "omega-ind-reflvl{0}.xdmf".format(idx), "w") as file:
         file.write_mesh(mesh)
@@ -162,6 +170,7 @@ for idx,mesh in enumerate(ls_mesh):
         file.write_mesh(mesh)
         file.write_function(B_ind)
 '''
+
 #V = FunctionSpace(mesh, ("CG", 1))
 #u_bc = Function(V)
 #left_facets = ft.indices[ft.values==left_marker]
@@ -170,3 +179,158 @@ for idx,mesh in enumerate(ls_mesh):
 
 
 
+def get_mesh_hierarchy_nonconvex(n_ref): 
+
+    gmsh.initialize()
+    gmsh.option.setNumber("Mesh.MeshSizeFactor", 2.5)
+    proc = MPI.COMM_WORLD.rank
+    top_marker = 2
+    bottom_marker = 1
+    bnd_marker = 1
+    omega_marker = 1
+    Bwithoutomega_marker = 2
+    rest_marker = 3 
+    if proc == 0:
+        # We create one rectangle for each subdomain
+
+        #r1 = gmsh.model.occ.addRectangle(0.25, 0, 0, 0.5, 0.5,tag=1)
+        #r2 = gmsh.model.occ.addRectangle(0.125, 0.0, 0, 0.75, 0.95,tag=2)
+        r1 = gmsh.model.occ.addRectangle(0.25, 0.05, 0, 0.5, 0.45,tag=1)
+        r2 = gmsh.model.occ.addRectangle(0.125, 0.05, 0, 0.75, 0.9,tag=2)
+        
+        r3 = gmsh.model.occ.addRectangle(0, 0, 0, 1, 1,tag=3)
+        gmsh.model.occ.fragment([(2,3)],[(2,1),(2,2)])
+
+        gmsh.model.occ.synchronize()
+        its = 0 
+        for surface in gmsh.model.getEntities(dim=2):
+            com = gmsh.model.occ.getCenterOfMass(surface[0], surface[1])
+            print(com)
+            if np.allclose(com, [0.5,0.275, 0]):
+                gmsh.model.addPhysicalGroup(2, [surface[1]], omega_marker)
+            elif np.allclose(com, [0.5, 0.5, 0]):
+                gmsh.model.addPhysicalGroup(2, [surface[1]], Bwithoutomega_marker )
+            else:
+                gmsh.model.addPhysicalGroup(2, [surface[1]], rest_marker)
+
+            #else:
+            #    gmsh.model.addPhysicalGroup(2, [surface[1]], omega_marker)
+        #    if np.allclose(com, [0.5,0.25, 0]):
+        #        bottom = surface[1]
+        #    else:
+        #        top = surface[1]
+        #gmsh.model.addPhysicalGroup(2, [bottom], bottom_marker)
+        #gmsh.model.addPhysicalGroup(2, [top], top_marker)
+        # Tag the left boundary
+        bnd_square = []
+        for line in gmsh.model.getEntities(dim=1):
+            com = gmsh.model.occ.getCenterOfMass(line[0], line[1])
+            if np.isclose(com[0], 0) or np.isclose(com[0], 1) or np.isclose(com[1], 0) or  np.isclose(com[1], 1): 
+                bnd_square.append(line[1])
+        gmsh.model.addPhysicalGroup(1, bnd_square, bnd_marker)
+        gmsh.model.mesh.generate(2)
+        gmsh.write("mesh.msh")
+        gmsh.finalize()
+
+    import meshio
+    def create_mesh(mesh, cell_type, prune_z=False):
+        cells = mesh.get_cells_type(cell_type)
+        cell_data = mesh.get_cell_data("gmsh:physical", cell_type)
+        points = mesh.points[:,:2] if prune_z else mesh.points
+        out_mesh = meshio.Mesh(points=points, cells={cell_type: cells}, cell_data={"name_to_read":[cell_data]})
+        return out_mesh
+
+    if proc == 0:
+        # Read in mesh
+        msh = meshio.read("mesh.msh")
+
+        # Create and save one file for the mesh, and one file for the facets
+        triangle_mesh = create_mesh(msh, "triangle", prune_z=True)
+        line_mesh = create_mesh(msh, "line", prune_z=True)
+        meshio.write("mesh.xdmf", triangle_mesh)
+        meshio.write("mt.xdmf", line_mesh)
+
+    #n_ref = 2 
+    #for i in range(n_ref): 
+
+    with XDMFFile(MPI.COMM_WORLD, "mesh.xdmf", "r") as xdmf:
+        mesh = xdmf.read_mesh(name="Grid")
+        ct = xdmf.read_meshtags(mesh, name="Grid")
+        mesh.topology.create_connectivity(mesh.topology.dim, mesh.topology.dim-1)
+    with XDMFFile(MPI.COMM_WORLD, "mt.xdmf", "r") as xdmf:
+        ft = xdmf.read_meshtags(mesh, name="Grid")
+
+    mesh_hierarchy = [] 
+    mesh_hierarchy.append(mesh) 
+
+    def refine_all(x):
+        return x[0] >= 0
+    for i in range(n_ref):
+        mesh.topology.create_entities(1)
+        cells = locate_entities(mesh, mesh.topology.dim, refine_all)
+        #print(cells)
+        if proc == 0:
+            edges = compute_incident_entities(mesh, cells, 2, 1)
+            print(edges)
+            mesh = refine(mesh, edges, redistribute=True)
+            mesh_hierarchy.append(mesh) 
+    return mesh_hierarchy
+
+'''
+ls_mesh = get_mesh_hierarchy_nonconvex(6)
+
+for idx,mesh in enumerate(ls_mesh):
+    with XDMFFile(mesh.comm, "mesh-nonconvex-reflvl{0}.xdmf".format(idx), "w") as file:
+        file.write_mesh(mesh)
+
+def omega_Ind(x):
+    
+    values = np.zeros(x.shape[1],dtype=ScalarType)
+    omega_coords = np.logical_and( ( x[0] >= 0.25 ), 
+                     np.logical_and(   (x[0] <= 0.75 ),
+                       np.logical_and(   (x[1]>= 0.05),  (x[1]<= 0.5)  )
+                     )
+                   ) 
+    rest_coords = np.invert(omega_coords)
+    values[omega_coords] = np.full(sum(omega_coords), 1.0)
+    values[rest_coords] = np.full(sum(rest_coords), 0)
+    return values
+
+def B_Ind(x):
+    values = np.zeros(x.shape[1],dtype=ScalarType)
+    # Create a boolean array indicating which dofs (corresponding to cell centers)
+    # that are in each domain
+    B_coords = np.logical_and( ( x[0] >= 0.125 ), 
+                 np.logical_and(   (x[0] <= 0.875 ),
+                   np.logical_and(   (x[1]>= 0.05),  (x[1]<= 0.95)  )
+                 )
+               ) 
+    rest_coords = np.invert(B_coords)
+    values[B_coords] = np.full(sum(B_coords), 1.0)
+    values[rest_coords] = np.full(sum(rest_coords), 0)
+    return values
+
+for idx,mesh in enumerate(ls_mesh):
+
+    Q_ind = FunctionSpace(mesh, ("DG", 0))
+    omega_ind = Function(Q_ind)
+    B_ind = Function(Q_ind)
+    B_ind.interpolate(B_Ind)
+    omega_ind.interpolate(omega_Ind)
+    
+    #cells_omega = locate_entities(mesh, mesh.topology.dim, omega_Ind)
+    #omega_ind.x.array[:] = 0.0
+    #omega_ind.x.array[cells_omega] = np.full(len(cells_omega), 1)
+
+    #cells_B = locate_entities(mesh, mesh.topology.dim, B_Ind)
+    #B_ind.x.array[:] = 0.0
+    #B_ind.x.array[cells_B] = np.full(len(cells_B), 1)
+
+    with XDMFFile(mesh.comm, "omega-ind-reflvl{0}.xdmf".format(idx), "w") as file:
+        file.write_mesh(mesh)
+        file.write_function(omega_ind)
+
+    with XDMFFile(mesh.comm, "B-ind-reflvl{0}.xdmf".format(idx), "w") as file:
+        file.write_mesh(mesh)
+        file.write_function(B_ind)
+'''
