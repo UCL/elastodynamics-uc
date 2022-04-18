c436cd1d23853fe18708792cacc96666a9afaecf
Janosch Preuss <janosch.preuss@stud.uni-goettingen.de>
2022-04-18 17:21:38 +0100

    add jump-sol for Helmholtz

--- scripts/helmholtz-UC.py
+++ scripts/helmholtz-UC.py
@@ -10,11 +10,11 @@ from ufl import ds, dx, grad, div,inner, jump, FacetNormal, dot, dS, Circumradiu
 from mpi4py import MPI
 from petsc4py.PETSc import ScalarType
 from math import log,pi
-from meshes import get_mesh_hierarchy, get_mesh_hierarchy_nonconvex
+from meshes import get_mesh_hierarchy, get_mesh_hierarchy_nonconvex,get_mesh_hierarchy_fitted_disc
 from dolfinx.io import XDMFFile
 
 # problem data. Todo: put into class
-kk = ScalarType(1)
+kk = ScalarType(10)
 k2 = kk**2
 
 order = 1
@@ -27,7 +27,7 @@ y_t = 1.0+tol
 
 
 ## convex case 
-'''
+
 def omega_Ind(x):
     
     values = np.zeros(x.shape[1],dtype=ScalarType)
@@ -52,9 +52,10 @@ def B_Ind(x):
     values[B_coords] = np.full(sum(B_coords), 1.0)
     values[rest_coords] = np.full(sum(rest_coords), 0)
     return values
-'''
 
-## convex-case 
+
+## non-convex case 
+'''
 def omega_Ind(x):
     
     values = np.zeros(x.shape[1],dtype=ScalarType)
@@ -81,6 +82,7 @@ def B_Ind(x):
     values[B_coords] = np.full(sum(B_coords), 1.0)
     values[rest_coords] = np.full(sum(rest_coords), 0)
     return values
+'''
 
 sigma_x = ScalarType(0.01)
 sigma_y = ScalarType(0.1)
@@ -89,18 +91,12 @@ y_s = ScalarType(1.0)
 
 def SolveProblem(msh,order=1,export_VTK=False,sol_which=1,add_bc=False): 
 
-    #msh = mesh.create_rectangle(comm=MPI.COMM_WORLD,
-    #                        points=((0.0, 0.0), (1.0, 1.0)), n=(N_mesh, N_mesh),
-    #                        #points=((-0.1, -0.1), (1.1, 1.1)), n=(N_mesh, N_mesh),
-    #                        cell_type=mesh.CellType.triangle,)
     
     n = FacetNormal(msh)
     h = CellDiameter(msh)
-    #h = 2.0*Circumradius(msh)
     
     x = ufl.SpatialCoordinate(msh)
 
-    Lu = lambda u : -div(grad(u)) - k2*u 
     
     if sol_which == 1:
         #pgamma = ScalarType(1e-5)
@@ -108,18 +104,49 @@ def SolveProblem(msh,order=1,export_VTK=False,sol_which=1,add_bc=False):
         ue = ufl.exp( -( (x[0] - x_s) ** 2 / (2*sigma_x)  + (x[1] - y_s) ** 2 / (2*sigma_y)  ) )
         f =   -ue * ( (x[0]-x_s)**2/ sigma_x**2  + (x[1]-y_s)**2 / sigma_y**2 - 1/sigma_x - 1/sigma_y + k2 )
         palpha = ScalarType(1e-5)
-    else: 
-        #pgamma = ScalarType(5e-3)
-        pgamma = ScalarType(5e-3)
-        #pgamma = ScalarType(1e-4)
+        mu = 1 
+    elif sol_which == 2:
+        pgamma = ScalarType(1e-3)
+        palpha = ScalarType(1e-2)
         #pgamma = ScalarType(1e-1)
+        #palpha = ScalarType(1e-0)
+        mu_t = 2
+        mu_b = 1 
+        eta = 0.6
+        b_eta = (mu_b/mu_t)*0.5*(1/eta)*kk*pi*np.cos(kk*pi*eta)
+        a_eta = np.sin(pi*kk*eta) - b_eta*eta**2
+        u_t = ufl.sin(kk*pi*x[0]) * ( a_eta + b_eta*x[1]*x[1] )
+        u_b = ufl.sin(kk*pi*x[0]) * ufl.sin(kk*pi*x[1])
+        #ue =   ufl.conditional( ufl.gt(x[1]-eta,0), 1.0 , 0.0) 
+        ue =   ufl.conditional( ufl.gt(x[1]-eta,0), u_t , u_b) 
+        mu =   ufl.conditional( ufl.gt(x[1]-eta,0),  mu_t , mu_b)
+        upper_ind = ufl.conditional( ufl.gt(x[1]-eta,0), 1.0 , 0.0) 
+        lower_ind = ufl.conditional( ufl.gt(x[1]-eta,0), 0.0 , 1.0) 
+        #ue = ufl.sin(kk*pi*x[0]) * ufl.sin(kk*pi*x[1])
+        #f =   -ue *  (-2*(kk*pi)**2 + k2) 
+        f =  ufl.conditional( ufl.gt(x[1]-eta,0),  (mu_t*(kk*pi)**2 - k2) * u_t - 2*b_eta*ufl.sin(kk*pi*x[0])*mu_t, (mu_b*2*(kk*pi)**2 - k2) * u_b ) 
+        def mu_Ind(x):
+            values = np.zeros(x.shape[1],dtype=ScalarType)
+            upper_coords = x[1] > eta 
+            lower_coords = np.invert(upper_coords)
+            values[upper_coords] = np.full(sum(upper_coords), mu_t)
+            values[lower_coords] = np.full(sum(lower_coords), mu_b)
+            return values
+    else:
+        mu = 1  
+        pgamma = ScalarType(5e-3)
+        palpha = ScalarType(5e-3)
+        #pgamma = ScalarType(1e-1)
+        #palpha = ScalarType(1e-0)
+        
         ue = ufl.sin(kk*pi*x[0]) * ufl.sin(kk*pi*x[1])
         #f = Lu(ue)
-        palpha = ScalarType(5e-3)
         #palpha = ScalarType(1e-2)
         f =   -ue *  (-2*(kk*pi)**2 + k2) 
 
      
+    #Lu = lambda u : -mu*div(grad(u)) - k2*u 
+    Lu = lambda u : -div(mu*grad(u)) - k2*u 
     p_cg_order = FiniteElement('CG', msh.ufl_cell(), order)
     #p_cg_order_dual = FiniteElement('CG', msh.ufl_cell(), order)
     mel = MixedElement([p_cg_order,p_cg_order])
@@ -158,33 +185,29 @@ def SolveProblem(msh,order=1,export_VTK=False,sol_which=1,add_bc=False):
     B_ind.interpolate(B_Ind)
     omega_ind.interpolate(omega_Ind)
 
-    #cells_omega = mesh.locate_entities(msh, msh.topology.dim, omega_Ind)
-    #omega_ind.x.array[:] = 0.0
-    #omega_ind.x.array[cells_omega] = np.full(len(cells_omega), 1)
-
-    #cells_B = mesh.locate_entities(msh, msh.topology.dim, B_Ind)
-    #B_ind.x.array[:] = 0.0
-    #B_ind.x.array[cells_B] = np.full(len(cells_B), 1)
-
-    #omega_ind = ufl.conditional(ufl.Not(ufl.And(ufl.And(x[0] >= 0.1, x[0] <= 0.9),ufl.And(x[1] >= 0.25, x[1] <= 1))   ), 1, 0) 
-    #B_ind = ufl.conditional( ufl.Not(ufl.And(ufl.And(x[0] >= 0.1, x[0] <= 0.9), ufl.And(x[1] >= 0.95, x[1] <= 1))) , 1, 0 ) 
-    #B_ind = ufl.conditional( ufl.Not(ufl.And(ufl.And(x[0] >= 0.1, x[0] <= 0.9), ufl.And(x[1] >= 0.9, x[1] <= 1))) , 1, 0 ) 
+   
+    if sol_which == 2:
+        mu = fem.Function(Q_ind)
+        mu.interpolate(mu_Ind)
     
-    #px = 1e+1
+
+    #px = 1e-2
     #input("") 
     a = omega_ind * u * v * dx
-    #a += pgamma*0.5*(h('+')+h('-'))*dot(jump(grad(u)),jump(grad(v)))*dS
-    a += pgamma*0.5*(h('+')+h('-'))*dot(jump(grad(u),n),jump(grad(v),n))*dS
+    a += pgamma*0.5*(h('+')+h('-'))*dot(jump(grad(u)*mu,n),jump(grad(v)*mu,n))*dS
+    #a += pgamma*0.5*(h('+')+h('-'))*dot(jump(grad(u)*mu),jump(grad(v)*mu))*dS
     a += pgamma * h**2 * Lu(u) * Lu(v) * dx 
-    #a += pgamma * h**2 * Lu(u) * Lu(v) * dx 
-    a += ( inner(grad(v), grad(z)) - k2*v*z) * dx 
+    #a += px * h**2 * Lu(u) * Lu(v) * dx 
+    a += ( mu * inner(grad(v), grad(z)) - k2*v*z) * dx 
     a -= inner(grad(z), grad(w)) * dx 
-    a += ( inner(grad(u), grad(w)) - k2*u*w) * dx 
+    a += ( mu *  inner(grad(u), grad(w)) - k2*u*w) * dx 
     a += palpha * h**(2*order) * u * v * dx
     
     #a += 1e-8 * u * v * dx
     #a += 1e-8 * w * z * dx
-
+    
+    #L = inner(f, w) * dx + omega_ind * ue  * v * dx  + pgamma * h**2 * inner(f,Lu(v)) * dx 
+    #L = inner(f, w) * dx + omega_ind * ue  * v * dx  + px * h**2 * inner(f,Lu(v)) * dx 
     L = inner(f, w) * dx + omega_ind * ue  * v * dx  + pgamma * h**2 * inner(f,Lu(v)) * dx 
 
     #problem = fem.petsc.LinearProblem(a, L, bcs=bcs, petsc_options={"ksp_type": "preonly", "pc_type": "lu"})
@@ -213,13 +236,27 @@ def SolveProblem(msh,order=1,export_VTK=False,sol_which=1,add_bc=False):
     #L2_error = fem.form( (omega_ind+B_ind)*inner(uh - ue, uh - ue) * dx)
     #L2_norm = fem.form((omega_ind+B_ind)*inner(ue, ue) * dx)
     
-    L2_error = fem.form( (B_ind)*inner(uh - ue, uh - ue) * dx)
-    L2_norm = fem.form((B_ind)*inner(ue, ue) * dx)
+    #L2_error = fem.form( (B_ind)*inner(uh - ue, uh - ue) * dx)
+    #L2_norm = fem.form((B_ind)*inner(ue, ue) * dx)
     
-    error_local = fem.assemble_scalar(L2_error)
-    L2_local = fem.assemble_scalar(L2_norm)
-    error_L2 = np.sqrt(msh.comm.allreduce(error_local, op=MPI.SUM)) / np.sqrt(msh.comm.allreduce(L2_local, op=MPI.SUM)) 
-    print("ndof = {0}, error_L2 = {1} ".format(ndof,error_L2)) 
+    if sol_which == 2:
+        L2_error_upper =  fem.form(  upper_ind * (B_ind)*inner(uh - ue, uh - ue) * dx)
+        L2_norm_upper = fem.form( upper_ind * (B_ind)*inner(ue, ue) * dx)
+        L2_error_lower =  fem.form(  lower_ind * (B_ind)*inner(uh - ue, uh - ue) * dx)
+        L2_norm_lower = fem.form( lower_ind * (B_ind)*inner(ue, ue) * dx)
+        L2_local_upper = fem.assemble_scalar(L2_norm_upper)
+        L2_local_lower = fem.assemble_scalar(L2_norm_lower)
+        error_local_upper = fem.assemble_scalar(L2_error_upper)
+        error_local_lower = fem.assemble_scalar(L2_error_lower)
+        error_L2 =  np.sqrt( msh.comm.allreduce(error_local_upper, op=MPI.SUM) + msh.comm.allreduce(error_local_lower, op=MPI.SUM) ) / np.sqrt( msh.comm.allreduce(L2_local_upper, op=MPI.SUM)+ msh.comm.allreduce(L2_local_lower, op=MPI.SUM)  ) 
+        print("ndof = {0}, error_L2 = {1} ".format(ndof,error_L2)) 
+    else:
+        L2_error = fem.form( (B_ind)*inner(uh - ue, uh - ue) * dx)
+        L2_norm = fem.form((B_ind)*inner(ue, ue) * dx)
+        error_local = fem.assemble_scalar(L2_error)
+        L2_local = fem.assemble_scalar(L2_norm)
+        error_L2 = np.sqrt(msh.comm.allreduce(error_local, op=MPI.SUM)) / np.sqrt(msh.comm.allreduce(L2_local, op=MPI.SUM)) 
+        print("ndof = {0}, error_L2 = {1} ".format(ndof,error_L2)) 
 
     if export_VTK:
         #V = VW.sub(0).collapse()[0]
@@ -241,10 +278,14 @@ def SolveProblem(msh,order=1,export_VTK=False,sol_which=1,add_bc=False):
         #print(V_to_VW)
         #input("")
         udiff.x.array[:] = np.abs(uh.x.array[V_to_VW] - uex.x.array)
+        #print(udiff.x.array)
         #udiff.x.array[:] = uh.x.array[V_to_VW]
         with XDMFFile(msh.comm, "helmholtz-UC.xdmf", "w") as file:
             file.write_mesh(msh)
             file.write_function(udiff)
+        with XDMFFile(msh.comm, "helmholtz-exact.xdmf", "w") as file:
+            file.write_mesh(msh)
+            file.write_function(uex)
         #with io.XDMFFile(msh.comm, "helmholtz-UC-uh.xdmf", "w") as file:
             #file.write_mesh(msh)
             #file.write_function(uh)
@@ -262,8 +303,9 @@ l2_errors = [ ]
 
 import matplotlib.pyplot as plt 
 #ls_mesh = get_mesh_hierarchy(6)
+ls_mesh = get_mesh_hierarchy_fitted_disc(7,eta=0.6)
 #ls_mesh = get_mesh_hierarchy(6)
-ls_mesh = get_mesh_hierarchy_nonconvex(6)
+#ls_mesh = get_mesh_hierarchy_nonconvex(6)
 
 l2_errors_order = { }
 eoc_order = {  }
@@ -276,9 +318,8 @@ for order in [1,2,3]:
     for msh in ls_mesh[:-order]:
         #l2_error, ndof = SolveProblem(msh=msh,order=order)
         #l2_error, ndof = SolveProblem(msh=msh,order=order, export_VTK=True,sol_which=2,add_bc=True)
-        l2_error, ndof = SolveProblem(msh=msh,order=order, export_VTK=True,sol_which=1,add_bc=True)
-        #l2_errors.append(SolveProblem(N_mesh=N_mesh,order=1, export_VTK=False,sol_which=2,add_bc=False))
-        #l2_errors.append(SolveProblem(msh=mesh,order=1, export_VTK=False,sol_which=2,add_bc=True))
+        #l2_error, ndof = SolveProblem(msh=msh,order=order, export_VTK=True,sol_which=2,add_bc=False)
+        l2_error, ndof = SolveProblem(msh=msh,order=order, export_VTK=True,sol_which=2,add_bc=False)
         l2_errors.append(l2_error)
         ndofs.append(ndof)
     eoc = [ log(l2_errors[i-1]/l2_errors[i])/log(2) for i in range(1,len(l2_errors))]
@@ -308,7 +349,6 @@ for order in [1,2,3]:
     tmp_str = "p={0}".format(order)
     tmp_str += ",eoc={:.2f}".format(eoc_order[order])
     plt.loglog(h_order[order],h_order[order]**eoc_order[order],label=tmp_str,linestyle='solid',color='gray')
-print("hello")
 plt.xlabel("h")
 plt.ylabel("L2-error")
 plt.legend()
